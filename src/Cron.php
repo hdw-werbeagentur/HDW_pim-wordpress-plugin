26189d2cec3aae8efbca625658b84a245255c54a
Anthony Spross <anthony@talented-monkeys.com>
2022-06-23 14:35:28 +0200

    add a second cronjob, one hour after the first

--- src/Cron.php
+++ src/Cron.php
@@ -17,7 +17,6 @@ class Cron
     {
         \add_action('wp', [$this, 'scheduleEvent']);
         \add_action('update_products', [$this, 'updateProducts']);
-        \add_action('update_stock', [$this, 'updateStock']);
     }
 
     /**
@@ -25,12 +24,12 @@ class Cron
      **/
     public function scheduleEvent(): void
     {
-        if (! wp_next_scheduled('update_stock')) {
-            wp_schedule_event(time(), 'hourly', 'update_stock');
+        if (!wp_next_scheduled('update_products')) {
+            wp_schedule_event(strtotime('00:00:00'), 'daily', 'update_products');
         }
 
-        if (! wp_next_scheduled('update_products')) {
-            wp_schedule_event(strtotime('00:00:00'), 'daily', 'update_products');
+        if (!wp_next_scheduled('update_products_repeat')) {
+            wp_schedule_event(strtotime('01:00:00'), 'daily', 'update_products_repeat');
         }
     }
 
